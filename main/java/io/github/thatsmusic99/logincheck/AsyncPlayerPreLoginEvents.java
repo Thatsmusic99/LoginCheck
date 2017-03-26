14448e4ebc4f667565db105fb06415dcad83ace9
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-03-26 11:10:14 +0100

    Added AsyncPlayerPreLoginEvents

--- main/java/io/github/thatsmusic99/logincheck/AsyncPlayerPreLoginEvents.java
+++ main/java/io/github/thatsmusic99/logincheck/AsyncPlayerPreLoginEvents.java
@@ -1,5 +1,22 @@
 package io.github.thatsmusic99.logincheck;
 
-public class AsyncPlayerPreLoginEvents {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+
+public class AsyncPlayerPreLoginEvents implements Listener {
+	
+	@EventHandler
+	public void onPlayerJoinPreventionEvent(AsyncPlayerPreLoginEvent e) {
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (player.hasPermission("logincheck.notify")) {
+				//
+			}
+		}
+	}
+	
+	
 
 }
